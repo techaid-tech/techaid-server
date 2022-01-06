b072df399dacee630c3c4b9a64c38a04377cc1f7
Marcin Bujar <marcin.bujar@gmail.com>
2022-01-06 21:30:04 +0000

    Refactor history table and trigger names

--- src/main/resources/db/migration/v21.05.19-1723__kits_history.sql
+++ src/main/resources/db/migration/v21.05.19-1723__kits_history.sql
@@ -1,7 +1,7 @@
-create sequence kits_audit_sequence start 1 increment 1;
+create sequence kits_history_sequence start 1 increment 1;
 
-create table kits_audit (
-    audit_id int8 not null default nextval('kits_audit_sequence'),
+create table kits_history (
+    entry_id int8 not null default nextval('kits_history_sequence'),
     id int8 not null,
     age int4 not null,
     donor_id int8,
@@ -15,16 +15,16 @@ create table kits_audit (
     coordinates jsonb,
     archived varchar(1) default false,
     organisation_id int8,
-    primary key (audit_id)
+    primary key (entry_id)
 );
 
-create or replace function kitauditfunc() returns trigger as $example_table$
+create function insert_kit_history_entry() returns trigger as $table$
    begin
-      insert into kits_audit(id, age, donor_id, created_at, location, model, status, type, updated_at, attributes, coordinates, archived, organisation_id)
+      insert into kits_history(id, age, donor_id, created_at, location, model, status, type, updated_at, attributes, coordinates, archived, organisation_id)
       values (old.id, old.age, old.donor_id, old.created_at, old.location, old.model, old.status, old.type, old.updated_at, old.attributes, old.coordinates, old.archived, old.organisation_id);
       return new;
    end;
-$example_table$ language plpgsql;
+$table$ language plpgsql;
 
-create trigger kit_audit_trigger after update on kits for each row execute procedure kitauditfunc();
+create trigger kits_history_trigger after update on kits for each row execute procedure insert_kit_history_entry();
 
